876e1b7f7213138b906dcb504803dd376f7b981d
santhosh776 <77959315+santhosh776@users.noreply.github.com>
2021-04-30 12:52:19 +0530

    Add files via upload

--- Quiz.js
+++ Quiz.js
@@ -29,39 +29,32 @@ class Quiz {
   }
 
   play(){
-    //write code here to hide question elements
-    
-    //write code to change the background color here
-    background("yellow")
-    //write code to show a heading for showing the result of Quiz
-    title2 = createElement('h1')
-    title2.html("Result of the Quiz");
-    title2.position(350, 0);
-    //call getContestantInfo( ) here
-    Contestant.getPlayerInfo()
-
-    //write condition to check if contestantInfor is not undefined
-    if(allContestants !== undefined)
-    {
-      fill("Blue")
-      textSize(30)
-       
-    }
-    //write code to add a note here
-    text("NOTE : contestant who answered correct are highlighted in green color !",130,230)
-    //write code to highlight contest who answered correctly
-    for(var plr in allContestants){
+    question.hide();
+    background("Yellow");
+    fill(0);
+    textSize(30);
+    text("Result of the Quiz",340, 50);
+    text("----------------------------",320, 65);
+    Contestant.getPlayerInfo();
+    if(allContestants !== undefined){
       debugger;
-      var correctAns = "2";
-      if (correctAns === allContestants[plr].answer)
-        fill("Green")
-      else
-        fill("red");
-
-      display_Answers+=30;
+      var display_Answers = 230;
+      fill("Blue");
       textSize(20);
-      text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers)
-    }
+      text("*NOTE: Contestant who answered correct are highlighted in green color!",130,230);
 
+      for(var plr in allContestants){
+        debugger;
+        var correctAns = "2";
+        if (correctAns === allContestants[plr].answer)
+          fill("Green")
+        else
+          fill("red");
+
+        display_Answers+=30;
+        textSize(20);
+        text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers)
+      }
+    }
   }
 }
